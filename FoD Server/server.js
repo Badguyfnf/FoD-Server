0a5cea3087509eada5beef4468c181bb85a0a4e2
Badguyfnf <116070144+Badguyfnf@users.noreply.github.com>
2026-09-04 17:59:34 +0300

    Add files via upload

--- FoD Server/server.js
+++ FoD Server/server.js
@@ -2,7 +2,7 @@ const { WebSocketServer } = require('ws');
 const port = process.env.PORT || 9080;
 const wss = new WebSocketServer({ port });
 
-let servers = {}; // { ws_id: { name, ws } }
+let servers = {}; // { ws_id: { name, players, max_players, ws } }
 let clients = {}; // { ws_id: ws }
 let idCounter = 1;
 
@@ -15,10 +15,25 @@ wss.on('connection', (ws) => {
             const data = JSON.parse(message);
             
             if (data.type === 'register') {
-                servers[wsId] = { name: data.name, ws: ws };
+                servers[wsId] = { 
+                    name: data.name || "Сервер", 
+                    players: data.players || 1,
+                    max_players: data.max_players || 3,
+                    ws: ws 
+                };
             } 
+            else if (data.type === 'update_server') {
+                if (servers[wsId]) {
+                    servers[wsId].players = data.players;
+                }
+            }
             else if (data.type === 'get_servers') {
-                const list = Object.keys(servers).map(id => ({ id: id, name: servers[id].name }));
+                const list = Object.keys(servers).map(id => ({ 
+                    id: id, 
+                    name: servers[id].name,
+                    players: servers[id].players || 1,
+                    max_players: servers[id].max_players || 3
+                }));
                 ws.send(JSON.stringify({ type: 'server_list', servers: list }));
             } 
             else if (['connect_request', 'offer', 'answer', 'candidate'].includes(data.type)) {
